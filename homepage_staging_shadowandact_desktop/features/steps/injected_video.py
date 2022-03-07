1e0b14f5f68bb2834273995f0b494e4798a63e31
Palak P Shah <dabbadolcha@gmail.com>
2022-03-08 01:21:09 +0530

    Initial commit

---
+++ homepage_staging_shadowandact_desktop/features/steps/injected_video.py
@@ -0,0 +1,103 @@
+from common import *
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+def load_initial_article_of_most_popular(driver):
+    print("function called load_initial_article_of_most_popular")
+    initial_article = driver.find_element(
+        By.XPATH,
+        "//div[@class='side-tabs']//ol//li[1]//a")
+    actions = ActionChains(driver)
+    actions.move_to_element(initial_article).perform()
+    title = initial_article.get_attribute("title")
+    initial_article.click()
+    WebDriverWait(driver, 40).until(ec.presence_of_element_located((
+        By.XPATH, "html/head/title")))
+    WebDriverWait(driver, 40).until(ec.title_is(title+" - SHADOW & ACT"))
+
+
+def verify_injected_video(driver):
+    print("function called verify_injected_video")
+    WebDriverWait(driver, 15).until(ec.presence_of_element_located((
+        By.XPATH, "(//div[@class='jwplayer image-wrapper image-wrapper--16x9'])[1]")))
+
+    # WebDriverWait(driver, 15).until(ec.presence_of_element_located((
+    #     By.XPATH, "(//div[@class='jwplayer image-wrapper image-wrapper--16x9']//div[@class='img']/div[1])[1]")))
+    # blank_img = driver.find_element(
+    #     By.XPATH,
+    #     "(//div[@class='jwplayer image-wrapper image-wrapper--16x9']//div[@class='img']/div[1])[1]")
+    # driver.execute_script("arguments[0].scrollIntoView();", blank_img)
+    # time.sleep(5)
+    # assert blank_img.is_displayed(), "Injected Video is not loaded for this article"
+
+    WebDriverWait(driver, 15).until(ec.presence_of_element_located((
+        By.XPATH, "//div[@class='jw-media jw-reset']")))
+    adv_and_video_container = driver.find_element(
+        By.XPATH,
+        "//div[@class='jw-media jw-reset']")
+    actions = ActionChains(driver)
+    actions.move_to_element(adv_and_video_container).perform()
+    WebDriverWait(driver, 15).until(ec.presence_of_element_located((
+        By.XPATH, "//video[@class='jw-video jw-reset']")))
+    video = driver.find_element(By.XPATH, "//video[@class='jw-video jw-reset']")
+    # time.sleep(5)
+    assert video.is_displayed(), "Video does not Autoplay when in view"
+    injected_video = driver.find_element(
+        By.XPATH,
+        "(//div[@class='jwplayer image-wrapper image-wrapper--16x9'])[1]")
+    actions = ActionChains(driver)
+    actions.move_to_element(injected_video).perform()
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located((
+        By.XPATH, "//video[@class='jw-video jw-reset']")))
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located((
+        By.XPATH, "//div[@class='jw-icon jw-icon-inline jw-button-color jw-reset jw-icon-playback']")))
+    within_video_play_btn = driver.find_elements(
+        By.XPATH, "//div[@class='jw-icon jw-icon-inline jw-button-color jw-reset jw-icon-playback']")
+    actions = ActionChains(driver)
+    actions.move_to_element(adv_and_video_container).perform()
+    assert within_video_play_btn[0].is_displayed(), "Injected Video does not Autoplay when in view"
+
+    # play_btn = driver.find_elements(
+    #     By.XPATH,
+    #     "(//button[@class='jwplayer__play bg-white color-at-green text-center'])[1]")
+    # assert not play_btn[0].is_displayed(), "Injected Video does not Autoplay when in view"
+
+
+def verify_pic_in_pic_window(driver):
+    body = driver.find_element(By.CSS_SELECTOR, "body")
+    body.send_keys(Keys.PAGE_DOWN)
+    print("page scrolled down once")
+    WebDriverWait(driver, 20).until(ec.presence_of_element_located((
+        By.XPATH,
+        "//div[@class='jw-overlays jw-reset']")))
+    pic_in_pic_player = driver.find_element(By.XPATH, "//div[@class='jw-overlays jw-reset']")
+    assert \
+        pic_in_pic_player.is_displayed(), \
+        "pic in pic jw player floating window is not present for injected video for article :" + driver.title
+    print("pic in pic floating window on page scroll of injected video is present")
+    # WebDriverWait(driver, 20).until(ec.presence_of_element_located((
+    #     By.XPATH,
+    #     "//div[@class='jw-icon jw-icon-inline jw-button-color jw-reset jw-icon-playback']")))
+    # pic_in_pic_adv = driver.find_element(By.XPATH, "(//video[@title='Advertisement'])[1]")
+    # actions = ActionChains(driver)
+    # actions.move_to_element(pic_in_pic_adv).perform()
+
+
+def verify_read_full_article(driver):
+    print("function called read_full_article")
+    read_full_article_btn = driver.find_element(
+        By.XPATH,
+        "(//button[@class='btn btn--primary bg-white'][normalize-space()='Read Full Article'])[1]")
+    actions = ActionChains(driver)
+    actions.move_to_element(read_full_article_btn).perform()
+    read_full_article_btn.click()
+    author = driver.find_element(
+        By.XPATH,
+        "(//div[@class='sa-article__meta sa-article__meta--bottom d-flex align-items-center font-secondary'])[1]")
+    WebDriverWait(driver, 20).until(ec.presence_of_element_located((
+        By.XPATH,
+        "(//div[@class='sa-article__meta sa-article__meta--bottom d-flex align-items-center font-secondary'])[1]")))
+    actions = ActionChains(driver)
+    actions.move_to_element(author).perform()
+    assert author.is_displayed(), "On Clicking Read Full Article button, Author is not displayed for article "+driver.title
+
---
+++ homepage_staging_shadowandact_desktop/features/steps/steps.py
@@ -0,0 +1,122 @@
+import time
+from behave import *
+from google_search_page import *
+from common_pages import *
+from common import *
+from injected_video import *
+
+
+@when('visit url "{url}"')
+def step(context, url):
+    context.browser.get(url)
+
+
+@when('field with name "{selector}" is given "{value}"')
+def step(context, selector, value):
+    elem = context.browser.find_element_by_name(selector)
+    elem.send_keys(value)
+    elem.submit()
+    time.sleep(5)
+
+
+@then('title becomes "{title}"')
+def step(context, title):
+    assert context.browser.title == title
+
+
+@then(u'page contains "{body}"')
+def step(context, body):
+    assert body in context.browser.page_source
+
+
+@given('the browser is launched and navigating to the Google.com page')
+def step_impl(context):
+    navigate_to_google_page(context.browser)
+
+
+@given('the browser is launched and load the staging.shadowandact.com app')
+def step_impl(context):
+    launch_browser_and_app(context.browser)
+
+
+@then('navigate to and load the initial article from most popular section of the home page')
+def step_impl(context):
+    load_initial_article_of_most_popular(context.browser)
+
+
+@then('verify if the injected video is auto playing')
+def step_impl(context):
+    verify_injected_video(context.browser)
+
+
+@then('verify if pic in pic window is appearing as required on page scroll')
+def step_impl(context):
+    verify_pic_in_pic_window(context.browser)
+
+
+@then('verify if Read Full Article Button is functioning as required')
+def step_impl(context):
+    verify_read_full_article(context.browser)
+
+
+@given('on android mobile the chrome browser is launched and navigating to the Google.com page')
+def step_impl(context):
+    navigate_to_google_page_on_mobile(context.browser)
+
+
+@given('on apple iphone mobile the safari browser is launched and navigating to the Google.com page')
+def step_impl(context):
+    navigate_to_google_page_on_mobile(context.browser)
+
+
+@then('navigate to the Google.com page')
+def step_impl(context):
+    navigate_to_google_page(context.browser)
+
+
+@then('search for "Shadow & Act" keyword')
+def step_impl(context):
+    search_keyword(context.browser, "Shadow & Act")
+
+
+@then('Navigate to the Shadow&Act application page from the Google search results')
+def step_impl(context):
+    launch_app(context.browser)
+
+
+@then('verify the application is launched successfully')
+def step_impl(context):
+    post_page_load_pop_up(context.browser)
+
+
+@then('navigate to the Film page')
+def step_impl(context):
+    verify_particular_page(context.browser, "FILM")
+
+
+@then('verify if all the article links are working as expected on the Film page')
+def step_impl(context):
+    verify_each_article(context.browser, "FILM", "Film")
+
+
+@then('verify if the Load More stories button work as expected, also verify the links to the articles works as expected')
+def step_impl(context):
+    verify_number_of_articles(context.browser, "Film")
+    verify_each_article(context.browser, "FILM", "Film")
+
+
+@then('verify if the Load More stories button work as expected (the second time when clicked), also verify initial link of the article, after clicking load more twice do work as expected')
+def step_impl(context):
+    verify_number_of_articles(context.browser, "Film")
+    verify_number_of_articles(context.browser, "Film")
+    verify_each_article(context.browser, "FILM", "Film")
+
+
+@then('verify footer section is present and displayed')
+def step_impl(context):
+    verify_footer_presence(context.browser)
+
+
+@then('close the browser')
+def step_impl(context):
+    context.browser.quit()
