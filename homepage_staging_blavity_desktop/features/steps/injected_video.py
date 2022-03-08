5dd90a61bec7d3a9e3538e8c6ceb28bab4f1f581
Palak P Shah <dabbadolcha@gmail.com>
2022-03-08 19:30:22 +0530

    video injection staging blavity

--- homepage_staging_blavity_desktop/features/steps/injected_video.py
+++ homepage_staging_blavity_desktop/features/steps/injected_video.py
@@ -2,63 +2,61 @@ from common import *
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-def load_initial_article_of_most_popular(driver):
+def load_initial_article_of_top_stories(driver):
     print("function called load_initial_article_of_most_popular")
     initial_article = driver.find_element(
         By.XPATH,
-        "//div[@class='side-tabs']//ol//li[1]//a")
+        "(//div[@class='home-top-story-card__title-container']//a)[1]")
     actions = ActionChains(driver)
     actions.move_to_element(initial_article).perform()
-    title = initial_article.get_attribute("title")
+    title = initial_article.text
     initial_article.click()
     WebDriverWait(driver, 40).until(ec.presence_of_element_located((
         By.XPATH, "html/head/title")))
-    WebDriverWait(driver, 40).until(ec.title_is(title+" - SHADOW & ACT"))
+    WebDriverWait(driver, 40).until(ec.title_is(title+" - Blavity News"))
 
 
 def verify_injected_video(driver):
     print("function called verify_injected_video")
+    # wait till the time main video screen is available
     WebDriverWait(driver, 15).until(ec.presence_of_element_located((
         By.XPATH, "(//div[@class='jwplayer image-wrapper image-wrapper--16x9'])[1]")))
-
-    # WebDriverWait(driver, 15).until(ec.presence_of_element_located((
-    #     By.XPATH, "(//div[@class='jwplayer image-wrapper image-wrapper--16x9']//div[@class='img']/div[1])[1]")))
-    # blank_img = driver.find_element(
-    #     By.XPATH,
-    #     "(//div[@class='jwplayer image-wrapper image-wrapper--16x9']//div[@class='img']/div[1])[1]")
-    # driver.execute_script("arguments[0].scrollIntoView();", blank_img)
-    # time.sleep(5)
-    # assert blank_img.is_displayed(), "Injected Video is not loaded for this article"
-
+    # wait till the time jw player of the main video screen is available
     WebDriverWait(driver, 15).until(ec.presence_of_element_located((
         By.XPATH, "//div[@class='jw-media jw-reset']")))
+    # look for container element having video and adv
     adv_and_video_container = driver.find_element(
         By.XPATH,
         "//div[@class='jw-media jw-reset']")
+    # move to the container element having video and adv
     actions = ActionChains(driver)
     actions.move_to_element(adv_and_video_container).perform()
+    # wait for the video to be present
     WebDriverWait(driver, 15).until(ec.presence_of_element_located((
         By.XPATH, "//video[@class='jw-video jw-reset']")))
     video_chk = driver.find_elements(By.XPATH, "//video[@class='jw-video jw-reset']")
+    # check for the video to be present
     if len(video_chk) > 0:
-        print("in if")
+        # if video is present
+        print("video is present")
+    else:
+        print("video not present")
         video = driver.find_element(By.XPATH, "//video[@class='jw-video jw-reset']")
         assert video.is_displayed(), "Video does not Autoplay when in view"
-    else:
-        print("in else")
-        video = driver.find_element(By.XPATH, "//video[@class='jw-video jw-reset']")
-        assert not video.is_displayed(), "Video does not Autoplay when in view"
+    # move to the video
     injected_video = driver.find_element(
         By.XPATH,
         "(//div[@class='jwplayer image-wrapper image-wrapper--16x9'])[1]")
     actions = ActionChains(driver)
     actions.move_to_element(injected_video).perform()
+    # wait till the time video auto plays when in view
     WebDriverWait(driver, 10).until(ec.presence_of_element_located((
         By.XPATH, "//video[@class='jw-video jw-reset']")))
     WebDriverWait(driver, 10).until(ec.presence_of_element_located((
         By.XPATH, "//div[@class='jw-icon jw-icon-inline jw-button-color jw-reset jw-icon-playback']")))
     within_video_adv = driver.find_elements(
         By.XPATH, "//div[@class='videoAdUiAutoClose']")
+    # check till the time video auto plays when in view
     if len(within_video_adv) > 0:
         print("image Adv present in injected video")
         actions = ActionChains(driver)
@@ -87,18 +85,19 @@ def verify_read_full_article(driver):
     print("function called read_full_article")
     read_full_article_btn = driver.find_element(
         By.XPATH,
-        "(//button[@class='btn btn--primary bg-white'][normalize-space()='Read Full Article'])[1]")
+        "(//button[@type='button'][normalize-space()='Read Full Article'])[1]")
     actions = ActionChains(driver)
     actions.move_to_element(read_full_article_btn).perform()
     read_full_article_btn.click()
-    author_chk = driver.find_elements(
+    read_comments_chk = driver.find_elements(
         By.XPATH,
-        "(//div[@class='sa-article__meta sa-article__meta--bottom d-flex align-items-center font-secondary'])[1]")
-    if len(author_chk) > 0:
-        print("Read Full Article Button working as expected")
+        "(//span[contains(text(),'Read Comments')])[1]")
+    if len(read_comments_chk) > 0:
+        print("Read Full Article Button is working as expected")
     else:
-        author = driver.find_element(
+        read_comments = driver.find_element(
             By.XPATH,
-            "(//div[@class='sa-article__meta sa-article__meta--bottom d-flex align-items-center font-secondary'])[1]")
-        assert author.is_displayed(), "On Clicking Read Full Article button, Author is not displayed for article "+driver.title
+            "(//span[contains(text(),'Read Comments')])[1]")
+        assert read_comments.is_displayed(), \
+            "On Clicking Read Full Article button, Read Comments is not displayed for article "+driver.title
 
